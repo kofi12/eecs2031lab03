24d4f8cde1db7f51a9919a31beb487cfb1cf34e5
kofi12 <kofi12@my.yorku.ca>
2018-10-15 22:29:22 +0000

    practice lab

---
+++ practicelab.c
@@ -0,0 +1,33 @@
+/*Name: Aaron Haizel
+  Student Number: 209619198
+*/
+
+#include <stdio.h>
+int isMirrored(int x);
+
+int main()
+{
+	int x, answ;
+	answ = 0;
+
+	printf("Give me a number\n");
+	scanf("%d", &x);
+	answ = isMirrored(x);
+
+	return 0;
+}
+
+int isMirrored(int x)
+{
+	char *f;
+	char *l;
+	char *n;
+	int i;
+
+	for(i = 0; *n = ((x % 10) - '0') && (x/=10); n++)
+		;;
+
+	for(i = 0, f = n, l = n; 
+
+	return 0;
+}
